18e0eba2060d1b7987156cb1bf65e3d0306e8544
Ahmad Syamim <ahmad.syamim@gmail.com>
2025-04-23 18:47:05 +0800

    update: bread default checkbox on id

--- resources/views/tools/bread/edit-add.blade.php
+++ resources/views/tools/bread/edit-add.blade.php
@@ -323,6 +323,7 @@
                                                 name="field_browse_{{ $data['field'] }}"
                                                 @if (isset($dataRow->browse) && $dataRow->browse) checked="checked"
                                                @elseif($data['key'] == 'PRI')
+                                               @elseif($data['field'] == 'id')
                                                @elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')
                                                @elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')
                                                @elseif(!isset($dataRow->browse))
@@ -331,22 +332,22 @@
                                                 for="field_browse_{{ $data['field'] }}">{{ __('voyager::generic.browse') }}</label><br />
                                             <input type="checkbox" id="field_read_{{ $data['field'] }}"
                                                 name="field_read_{{ $data['field'] }}"
-                                                @if (isset($dataRow->read) && $dataRow->read) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->read)) checked="checked" @endif>
+                                                @if (isset($dataRow->read) && $dataRow->read) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['field'] == 'id')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->read)) checked="checked" @endif>
                                             <label
                                                 for="field_read_{{ $data['field'] }}">{{ __('voyager::generic.read') }}</label><br />
                                             <input type="checkbox" id="field_edit_{{ $data['field'] }}"
                                                 name="field_edit_{{ $data['field'] }}"
-                                                @if (isset($dataRow->edit) && $dataRow->edit) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['type'] == 'timestamp' && $data['field'] == 'created_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->edit)) checked="checked" @endif>
+                                                @if (isset($dataRow->edit) && $dataRow->edit) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['field'] == 'id')@elseif($data['type'] == 'timestamp' && $data['field'] == 'created_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->edit)) checked="checked" @endif>
                                             <label
                                                 for="field_edit_{{ $data['field'] }}">{{ __('voyager::generic.edit') }}</label><br />
                                             <input type="checkbox" id="field_add_{{ $data['field'] }}"
                                                 name="field_add_{{ $data['field'] }}"
-                                                @if (isset($dataRow->add) && $dataRow->add) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['type'] == 'timestamp' && $data['field'] == 'created_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->add)) checked="checked" @endif>
+                                                @if (isset($dataRow->add) && $dataRow->add) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['field'] == 'id')@elseif($data['type'] == 'timestamp' && $data['field'] == 'created_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->add)) checked="checked" @endif>
                                             <label
                                                 for="field_add_{{ $data['field'] }}">{{ __('voyager::generic.add') }}</label><br />
                                             <input type="checkbox" id="field_delete_{{ $data['field'] }}"
                                                 name="field_delete_{{ $data['field'] }}"
-                                                @if (isset($dataRow->delete) && $dataRow->delete) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->delete)) checked="checked" @endif>
+                                                @if (isset($dataRow->delete) && $dataRow->delete) checked="checked" @elseif($data['key'] == 'PRI')@elseif($data['field'] == 'id')@elseif($data['type'] == 'timestamp' && $data['field'] == 'updated_at')@elseif($data['type'] == 'timestamp' && $data['field'] == 'deleted_at')@elseif(!isset($dataRow->delete)) checked="checked" @endif>
                                             <label
                                                 for="field_delete_{{ $data['field'] }}">{{ __('voyager::generic.delete') }}</label><br />
                                         </div>
